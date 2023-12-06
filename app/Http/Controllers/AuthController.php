fcd1c9037c1a94a25c011aa74a8b3968395e5d86
Danny Pacheco <135442481+dapacheco1espe@users.noreply.github.com>
2023-12-05 21:30:25 -0500

    feat:add passport configuration
    - in addition change .env.example

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function signUp(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json([
+            'message' => 'Successfully created user!'
+        ], 201);
+    }
+  
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+            'remember_me' => 'boolean'
+        ]);
+        $credentials = request(['email', 'password']);
+        if (!Auth::attempt($credentials))
+            return response()->json([
+                'message' => 'Incorrect credentials'
+            ], 400);
+        $user = $request->user();
+        $user_token['token'] = $user->createToken('appToken')->accessToken;
+        return response()->json([
+            'success' => true,
+            'token' => $user_token['token'],
+            'user' => $user,
+        ], 200);
+    }
+  
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+}
